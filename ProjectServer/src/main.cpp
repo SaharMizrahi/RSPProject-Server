2e087c9789348426c72975280e9f5c81c9230055
user <user@Ubuntu-VM>
2017-08-22 13:57:38 +0300

    encrypt and decrypt

--- ProjectServer/src/main.cpp
+++ ProjectServer/src/main.cpp
@@ -1,55 +1,36 @@
+#include <Protocol.h>
 #include<iostream>
 #include<stdio.h>
-#include "RSPServer.h"
 #include "TCPSocket.h"
 #include "RSPGameClient.h"
 #include "RSPGameServer.h"
 #include "MThread.h"
-#include "LoginProtocol.h"
 #define IP "127.0.0.1"
+#include "Encryptor.h"
 using namespace std;
 using namespace networkingLab;
 
 int main() {
 
-	/*RSPServer* server=new RSPServer(SERVER_PORT);
-	server->start();*/
+
 	RSPGameServer* server=new RSPGameServer(SERVER_PORT);
 	server->start();
 	sleep(1);
-	RSPGameClient *client1=new RSPGameClient("sahar","sahar");
+	RSPGameClient *client1=new RSPGameClient("gal","gal");
 	client1->start();
-
+	sleep(10);
+	RSPGameClient *client2=new RSPGameClient("sahar","sahar");
+	client2->start();
 	client1->waitForThread();
-	/*TCPSocket* s=new TCPSocket(SERVER_IP,SERVER_PORT);
-	char buffer[]="login sahar sahar";
-	int size=htonl(sizeof(buffer));
-	s->write((char*)&size, 4);
-	s->write(buffer, size);
-	int serverAnswer;
-	s->read((char*)&serverAnswer, 4);
-	switch(ntohl(serverAnswer))
-	{
-	case 0:
-	case 3:
-		int newPort;
-		s->read((char*)&newPort, 4);
-		sleep(2);
-		s=new TCPSocket(IP,ntohl(newPort));
 
-		char buf[]="2";
-		int bufSize=htonl(sizeof(buf));
-		s->write((char*)&bufSize, 4);
-		s->write(buf, bufSize);
-		sleep(5);
-		s->read((char*)&bufSize, 4);
-		s->read(buffer, ntohl(bufSize));
-		sleep(4);
-		s->close();
-		break;
+	/*Encryptor* e=new Encryptor();
+
+	cout<<e->encrypt("gal")<<endl;
+	cout<<e->encrypt("sahar")<<endl;
+	cout<<e->encrypt("gal")<<endl;*/
 
 
-	}*/
+
 
 
 
