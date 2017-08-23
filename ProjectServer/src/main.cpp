bb7577f277e59bca3b93ddecb673c5bc2fc87e87
user <user@Ubuntu-VM>
2017-08-23 16:11:58 +0300

    add some error points, fix updating rank

--- ProjectServer/src/main.cpp
+++ ProjectServer/src/main.cpp
@@ -1,34 +1,19 @@
 #include <Protocol.h>
 #include<iostream>
 #include<stdio.h>
-#include "TCPSocket.h"
 #include "RSPGameClient.h"
 #include "RSPGameServer.h"
-#include "MThread.h"
 #define IP "127.0.0.1"
-#include "Encryptor.h"
 using namespace std;
 using namespace networkingLab;
 
 int main() {
-
-
-	RSPGameServer* server=new RSPGameServer(SERVER_PORT);
+	RSPGameServer*  server=new RSPGameServer(SERVER_PORT);
 	server->start();
-	sleep(1);
-	RSPGameClient *client1=new RSPGameClient("gal","gal");
-	client1->start();
-	sleep(10);
-	RSPGameClient *client2=new RSPGameClient("sahar","sahar");
-	client2->start();
-	client1->waitForThread();
-
-	/*Encryptor* e=new Encryptor();
-
-	cout<<e->encrypt("gal")<<endl;
-	cout<<e->encrypt("sahar")<<endl;
-	cout<<e->encrypt("gal")<<endl;*/
-
+	/*sleep(2);
+	RSPGameClient* client=new RSPGameClient("sahar","sahar","login");
+	client->start();*/
+	sleep(1000);
 
 
 
